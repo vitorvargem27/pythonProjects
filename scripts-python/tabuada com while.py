9947ce1955124944f783023f615bd76f2b69637d
vitorVargem <tp.vitor.vargem@gmail.com>
2023-12-02 22:05:50 -0300

    dictionary Time included

--- scripts-python/tabuada com while.py
+++ scripts-python/tabuada com while.py
@@ -1,3 +1,4 @@
+import time
 count = 0
 number = 0
 total = 0
@@ -10,6 +11,7 @@ while number >= 0 :
     for count in range(0, 10):
         count += 1
         total = count * number
+        time.sleep(1)
         print(f'{number} x {count} = {total}')
 
 print('\033[1:31mEnd of program because you wrote a negative number \033[m')
