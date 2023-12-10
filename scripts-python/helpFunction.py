8588b6c93377970dee76eabbf506e1b5380ce97f
vitorVargem <tp.vitor.vargem@gmail.com>
2023-12-10 16:23:21 -0300

    Help Function Updated

--- scripts-python/helpFunction.py
+++ scripts-python/helpFunction.py
@@ -1,14 +1,16 @@
-def interativeHelp(analysisParameter) :
-    print(f"\033[1:46m{analysisParameter:>10}" + (' ' * 5) + '\033[m')
-    print("\033[1:44m~\033[m" * 33)
-    print(f"\033[1:36:44m{analysisParameter:>10} command access!! " + (' ' * 5) + "\033[m")
-    print("\033[1:44m~\033[m" * 33)
-    help(analysisParameter)
+def interativeHelp() :
+    program = True
+    while program :
+        functionParameter = str(input('\033[1:36:42mWhat function are you want analise?\033[m\n')).strip().lower()
+        print(f"\033[1:46m{functionParameter:>10}" + (' ' * 5) + '\033[m')
+        print("\033[1:44m~\033[m" * 33)
+        print(f"\033[1:36:44m{functionParameter:>10} command access!! " + (' ' * 5) + "\033[m")
+        print("\033[1:44m~\033[m" * 33)
+        help(functionParameter)
 
-program = True
-while program :
-    functionParameter = str(input('\033[1:36:42mWhat function are you want analise?\033[m\n')).strip().lower()
-    interativeHelp(functionParameter)
+        if functionParameter == 'fim' :
+            print('FIM DE JOGO')
+            break
 
-    if functionParameter == 'fim' :
-        break
+
+interativeHelp()
