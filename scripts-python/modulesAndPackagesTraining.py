c9bf54d89d54abfa5ab2159418c63d3c0c8863ff
vitorVargem <tp.vitor.vargem@gmail.com>
2023-12-12 14:28:33 -0300

    Updated modules name

--- scripts-python/modulesAndPackagesTraining.py
+++ scripts-python/modulesAndPackagesTraining.py
@@ -1,17 +1,17 @@
-from functionsUtilities import dado
-from functionsUtilities import moeda
+from functionsUtilities import data
+from functionsUtilities import coins
 
 number = float(input('Write a number in R$ for consult the math situations : \n').strip())
 value = float(input('Write a value in R$ for add in the original value :\n').strip())
 choose = str(input('You want see a real coin?[Y]/[N]\n')).strip().upper()
 boolean = ''
 
-decisionCoin = moeda.decision(choose)
-sumOfValues = dado.increase(number, value, boolean)
-subtraction = dado.decrease(number, value, boolean)
-double = dado.doubleValue(number, boolean)
-halfNumber = dado.halfValue(number, boolean)
+decisionCoin = coins.decision(choose)
+sumOfValues = data.increase(number, value, boolean)
+subtraction = data.decrease(number, value, boolean)
+double = data.doubleValue(number, boolean)
+halfNumber = data.halfValue(number, boolean)
 
-dado.resume(decisionCoin, number, value, sumOfValues, subtraction, double, halfNumber)
+data.resume(decisionCoin, number, value, sumOfValues, subtraction, double, halfNumber)
 
 
